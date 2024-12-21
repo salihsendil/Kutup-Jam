b3ebf32579a82753b9d35138c9294cee40df6797
gmzalbsn <gamzealbsn@gmail.com>
2024-12-21 06:33:55 +0300

    feat:Added enemy spawner

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,39 +1,44 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform target; 
-    private NavMeshAgent agent; 
-
-    void Start()
+    private NavMeshAgent agent;
+    public Transform player;
+    public int health;
+    private void Awake()
     {
-        agent = GetComponent<NavMeshAgent>();
-        
-        agent.updateRotation = false;
-        agent.updateUpAxis = false;
-
-        // Hedefe doğru hareket et
-        if (target != null)
+        StartCoroutine(AssignPlayer());
+    }
+    private IEnumerator AssignPlayer()
+    {
+        while (GameManager.Instance == null || GameManager.Instance.GetPlayer() == null)
         {
-            agent.SetDestination(target.position);
+            yield return null;
         }
+
+        player = GameManager.Instance.GetPlayer().transform;
     }
 
-    void Update()
+    private void Start()
     {
-        // Hedef sürekli değişiyorsa, hedef konumunu güncelle
-        if (target != null)
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+    private void Update()
+    {
+        if (player != null)
         {
-            agent.SetDestination(target.position);
-        }
-
-        // Düşmanı hedefe doğru çevir (isteğe bağlı)
-        Vector3 direction = (target.position - transform.position).normalized;
-        if (direction.magnitude > 0.1f)
-        {
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            agent.SetDestination(player.position);
+            Vector3 direction = (player.position - transform.position).normalized;
+            if (direction.magnitude > 0.1f)
+            {
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            }
         }
     }
 }
